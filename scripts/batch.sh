14385866d8368e51fca77df4fe36545b504ec4be
Joao Gabriel Lofiego <jg.lofiego@gmail.com>
2026-05-28 14:34:58 -0300

    Delete: Delete all files that are not raw results

--- scripts/batch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-script="./main.sh"
-create_folders="./smaller_scripts/create_folders.sh"
-
-echo "Running script 1 = create_folders.sh"
-bash "$create_folders"
-
-nohup "$script" 501 502 503 504 505 A 216 347 352 464 &> "./logs/batch_small_instances.txt" &
--- src/comolsd/headers/global_modules/local_search/pareto_ls.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARETO_LS_H
-#define PARETO_LS_H
-
-#include "../adaptative_grid/ParetoSetLS.h"
-#include "./neighborhood.h"
-
-vector<Solution *> * pareto_ls(vector<Solution*> s);
-
-#endif
